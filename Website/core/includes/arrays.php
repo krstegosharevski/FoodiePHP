dd37728ced3a9cb496dfe298880fa0999404df87
krstegosharevski <krsste.gosarevski@outlook.com>
2024-05-24 01:39:30 +0200

    code menu page

--- Website/core/includes/arrays.php
+++ Website/core/includes/arrays.php
@@ -42,4 +42,50 @@
         ),
     );
 
+    //Menu Items
+    $menuItems = array(
+        "club-sandwich" => array(
+            "title" => "Club Sandwich",
+            "price" => 11,
+            "blurb" => "Indulge in our Classic Club Sandwich, 
+                a triple-layer delight stacked with succulent 
+                roasted turkey, crispy bacon, fresh lettuce, 
+                and juicy tomatoes, all nestled between perfectly 
+                toasted bread ",
+            "drink" => "Club Soda"
+        ),
+        "dill-salmon" => array(
+            "title" => "Lemon &amp; Dill Salmon",
+            "price" => 18,
+            "blurb" => "Savor the exquisite flavors of our Dill Salmon, 
+                featuring a perfectly seared fillet infused with a fragrant 
+                blend of fresh dill, zesty lemon, and a hint of garlic. 
+                This succulent salmon is cooked to perfection, offering a 
+                tender, flaky texture that melts in your mouth. ",
+            "drink" => "Fancy Wine"
+        ),
+        "super-salad" => array(
+            "title" => "The Super Salad<sup>&reg;</sup>",
+            "price" => 34,
+            "blurb" => "Experience a burst of freshness with our Super Salad, 
+                a vibrant medley of the season's finest ingredients. 
+                This nutrient-packed salad features a colorful array 
+                of crisp greens, juicy cherry tomatoes, crunchy cucumbers, 
+                and sweet bell peppers. Tossed with protein-rich quinoa, 
+                creamy avocado, and a sprinkle of tangy feta cheese, 
+                each bite is a delightful combination of textures and 
+                flavors.",
+            "drink" => "Jug o' Water"
+        ),
+        "mexican-barbacoa" => array(
+            "title" => "Mexican Barbacoa",
+            "price" => 21,
+            "blurb" => "Dive into the rich, authentic flavors of our Mexican Barbacoa, 
+                a traditional dish that’s sure to tantalize your taste buds. Our barbacoa 
+                features tender, slow-cooked beef, marinated in a robust blend of smoky 
+                chipotle peppers, earthy cumin, and aromatic garlic.",
+            "drink" => "Corona"
+        ),
+    )
+
 ?>
