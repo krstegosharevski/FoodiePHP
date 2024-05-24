579e355fb16f16d7e1fca56b3635730b07e9cdb9
krstegosharevski <krsste.gosarevski@outlook.com>
2024-05-24 13:03:48 +0200

    $_GET dish.php & created contact page

---
+++ Website/core/contact.php
@@ -0,0 +1,35 @@
+<?php
+
+    const TITLE = "Contact Us | Frankline's Fine Dining";
+
+    include ('includes/header.php');
+
+?>
+
+<div id="contact">
+
+    <hr>
+
+    <h1>Get in touch with us!</h1>
+
+    <form method="post" action="" id="contact-form">
+
+        <label for="name">Your name</label>
+        <input type="text" id="name" name="name">
+
+        <label for="email">Your email</label>
+        <input type="email" id="email" name="email">
+
+        <label for="message">and your message</label>
+        <textarea id="message" name="message"></textarea>
+
+        <input type="checkbox" id="subscribe" name="subscribe" value="Subscribe">
+        <label for="subscribe">Subscribe to newsletter</label>
+
+        <input type="submit" class="button next" name="contact_submit" value="Send Message">
+    </form>
+</div> <!-- contact -->
+
+<?php
+    include ('includes/footer.php');
+?>
