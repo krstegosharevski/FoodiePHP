48c54747b59bff86b526ca4e6fb10c0956b975c1
krstegosharevski <krsste.gosarevski@outlook.com>
2024-05-24 14:16:32 +0200

    finalization of the project

--- Website/core/contact.php
+++ Website/core/contact.php
@@ -12,6 +12,65 @@
 
     <h1>Get in touch with us!</h1>
 
+    <?php
+        //Check for header injections
+        function has_header_injection($str){
+            return preg_match("/[\r\n]/", $str);
+        }
+
+        if (isset($_POST['contact_submit'])){
+            $name = trim($_POST['name']);
+            $email = trim($_POST['email']);
+            $msg = $_POST['message'];
+
+            //Check to see if $name or $email have header injections
+            if (has_header_injection($name) || has_header_injection($email)){
+                die(); //If true, kill the script.
+            }
+            if (!$name || !$email || !$msg){
+                echo '<h4 class="error">All fields required.</h4>
+                        <a href="contact.php" class="button block">Go back and try again</a>';
+                exit;
+            }
+
+            //And the recipient email to a variable
+            $to = "yourmail@gamil.com";
+
+            //Create a subject
+            $subject = "$name sent you a message via your contact form";
+
+            //Construct the message
+            $message = "Name: $name\r\n";
+            $message .= "Email: $email\r\n";
+            $message .= "Message:\r\n$msg";
+
+            //If the subscribe checkbox was checked..
+            if (isset($_POST['subscribe']) && $_POST['subscribe'] == 'Subscribe'){
+
+                //Add a new line to the $message
+                $message .= "\r\n\r\nPlease add $email to the mailing list.\r\n";
+            }
+            $message  = wordwrap($message, 72);
+
+            // Set the mail headers into a variable
+            $headers = "MIME-Version: 1.0\r\n";
+            $headers .= "Contact-type: text/plain; charset=iso-8859-1\r\n";
+            $headers .= "From: $name <$email>\r\n";
+            $headers .= "X-Priority: 1\r\n";
+            $headers .= "X-MsMail-Priority: High\r\n\r\n";
+
+            //Send the email!
+            mail($to, $subject, $message, $headers);
+
+    ?>
+
+        <!-- show success message -->
+        <h5>Thanks for contracting Franklin's!</h5>
+        <p>Please allow 24 hours for a response.</p>
+        <p><a href="index.php" class="button block">&laquo; Go to Home Page</a></p>
+
+    <?php } else{?>
+
     <form method="post" action="" id="contact-form">
 
         <label for="name">Your name</label>
@@ -28,6 +87,8 @@
 
         <input type="submit" class="button next" name="contact_submit" value="Send Message">
     </form>
+    <?php } ?>
+    <hr>
 </div> <!-- contact -->
 
 <?php
